b1f12dbfb044a13f4deab24e9b46ba92f88cc9db
Dinesh Uthayakumar <udinesh5@gmail.com>
2024-11-12 11:10:22 +0530

    Added Plugin Tracer for generic tracing of messages

---
+++ PluginTracer.cs
@@ -0,0 +1,37 @@
+using Microsoft.Xrm.Sdk;
+using System;
+using DataverseExperiments.Helper;
+
+namespace DataverseExperiments
+{
+    /// <summary>
+    /// A plugin that restricts the update of description field on the contact table.
+    /// This plugin is registered on the Pre-Operation stage of the Create and Update event pipeline for the contact table.
+    /// </summary>
+    public class PluginTracer : PluginBase
+    {
+        public PluginTracer(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(PluginTracer))
+        {
+        }
+
+        protected override void ExecuteDataversePlugin(ILocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null)
+            {
+                throw new ArgumentNullException(nameof(localPluginContext));
+            }
+            IPluginExecutionContext context = localPluginContext.PluginExecutionContext;
+
+            localPluginContext.Trace($"Message Name: {context.MessageName}");
+            localPluginContext.Trace($"Stage: {context.Stage}");
+            localPluginContext.Trace($"Mode: {context.Mode}"); 
+            localPluginContext.Trace($"Primary Entity Name: {context.PrimaryEntityName}");
+            localPluginContext.Trace($"Secondary Entity Name: {context.SecondaryEntityName}");
+            localPluginContext.Trace($"Primary Entity Id: {context.PrimaryEntityId}");
+            localPluginContext.Trace($"Initiating User Id: {context.InitiatingUserId}");
+            localPluginContext.Trace($"Correlation Id: {context.CorrelationId}");
+            localPluginContext.Trace($"Operation Id: {context.OperationId}");
+        }
+    }
+}
